e0e7153578a2eb36e556611ab90e3dac08c11b9b
Simon Mika <simon@mika.se>
2019-05-23 16:37:10 +0200

    Improved verification.

--- Verifier.ts
+++ Verifier.ts
@@ -10,10 +10,15 @@ export class Verifier {
 		for (const algorithm of algorithms)
 			this.algorithms[algorithm.name] = algorithm
 	}
-	verify(data: string | Token): Payload | undefined {
-		const splitted = data.split(".", 3)
+	verify(token: string | Token): Payload | undefined {
+		const splitted = token.split(".", 3)
 		const header: Header = JSON.parse(base64Url.decode(splitted[0]))
 		const algorithm = this.algorithms[header.alg]
-		return algorithm && algorithm.verify(data, splitted[2]) ? JSON.parse(base64Url.decode(splitted[1])) : undefined
+		const result: Payload | undefined = algorithm && algorithm.verify(token, splitted[2]) ? JSON.parse(base64Url.decode(splitted[1])) as Payload : undefined
+		const now = Date.now()
+		return result && (result.exp == undefined || result.exp > now) && (result.iat == undefined || result.iat <= now) ? result : undefined
+	}
+	authenticate(value: string): Payload | undefined {
+		return req.headers.authorization && req.headers.authorization.startsWith("Bearer ") ? verifier.verify(req.headers.authorization.substr(7)) : undefined
 	}
 }
