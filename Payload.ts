b68552c4085efdf8975a01d39aba0c170a6b3ec4
Simon Mika <simonmika@users.noreply.github.com>
2019-05-28 09:27:58 +0200

    Handling of undefined. (#3)
    * Added handling of undefined.
    
    * Allowed nested payloads.

--- Payload.ts
+++ Payload.ts
@@ -1,8 +1,11 @@
-export interface Payload {
+export interface Payload extends Data {
 	sub?: string // subject
 	iss?: string // issuer
 	aud?: string | string[] // audience
 	exp?: number // expires at
 	iat?: number // issued at
-	[claim: string]: string | string[] | number | undefined
 }
+export interface Data {
+	[claim: string]: Value | Value[] | undefined
+}
+export type Value = string | number | Data
