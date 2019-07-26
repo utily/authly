380a1db5043891bd3a27fe582750b34bb2c5d7f6
Simon Mika <simonmika@users.noreply.github.com>
2019-07-26 16:06:10 +0200

    Created indentifier.

--- RandomEncoder.ts
+++
@@ -1,8 +0,0 @@
-import { crypto } from "./crypto"
-import * as Base64 from "./Base64"
-
-export namespace RandomEncoder {
-	export function encodeRandom(length: number): string {
-		return Base64.encode(crypto.getRandomValues(new Uint8Array(length)))
-	}
-}
