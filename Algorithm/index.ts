0e00fbbe169321a9195366c9a175e5624a500b84
Simon Mika <simonmika@users.noreply.github.com>
2019-08-14 18:06:00 +0200

    Added RS256 algorithm. (#25)

--- Algorithm/index.ts
+++ Algorithm/index.ts
@@ -4,6 +4,7 @@ import { None } from "./None"
 import { HS256 } from "./HS256"
 import { HS384 } from "./HS384"
 import { HS512 } from "./HS512"
+import { RS256 } from "./RS256"
 
 export {
 	Name,
@@ -12,4 +13,5 @@ export {
 	HS256,
 	HS384,
 	HS512,
+	RS256,
 }
