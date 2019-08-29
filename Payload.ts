e52fac985c8d8bee7d64e879ac59cecf63937034
Simon Mika <simonmika@users.noreply.github.com>
2019-08-29 13:17:46 +0200

    Exported Payload parts. (#26)

--- Payload.ts
+++ Payload.ts
@@ -1,11 +1,13 @@
-export interface Payload extends Data {
+export interface Payload extends Payload.Data {
 	sub?: string // subject
 	iss?: string // issuer
 	aud?: string | string[] // audience
 	exp?: number // expires at
 	iat?: number // issued at
 }
-export interface Data {
-	[claim: string]: Value | Value[] | undefined
+export namespace Payload {
+	export interface Data {
+		[claim: string]: Value | Value[] | undefined
+	}
+	export type Value = string | number | Data
 }
-export type Value = string | number | Data
