bdeb2b938651841d2f864dbc9d44e7457e1e1d89
Robin Nedergård <robin.nedergard@payfunc.com>
2020-04-03 12:45:37 +0200

    removed test requiring standard signers

--- Verifier.spec.ts
+++ Verifier.spec.ts
@@ -38,29 +38,4 @@ describe("Verifier", () => {
 		expect(verifier && await verifier.verify(base64url)).toBeTruthy()
 		expect(verifier && await verifier.verify(base64std)).toBeTruthy()
 	})
-	it("Verifying both standard base64 encoded and url base 64 encoded jwt with old and new issuers.", async () => {
-		const json = {
-			"url": "https://example.com?param1=123",
-			"url2": "https://example.com?param1=321",
-		}
-		const jwtUrl = validIssuer && await validIssuer.sign(json, 1570094329996)
-		const jwtStandard = validIssuer && await validIssuer.sign(json, 1570094329996, true)
-		const base64url = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJhdWRpZW5jZSIsImlhdCI6MTU3MDA5NDMyOTk5NiwidXJsIjoiaHR0cHM6Ly9leGFtcGxlLmNvbT9wYXJhbTE9MTIzIiwidXJsMiI6Imh0dHBzOi8vZXhhbXBsZS5jb20_cGFyYW0xPTMyMSJ9.D-QL6NX4Te8lNv2_aQlHRa8ETMuM4emKSGaMBo48r7s"
-		// Sign part in base64std is already url encoded since before. This test is for checking backwards compatibility with a previous incorrect use of base64 standard encoding when signing and verifying data (except for the last "sign" part of jwt).
-		const base64std = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJhdWRpZW5jZSIsImlhdCI6MTU3MDA5NDMyOTk5NiwidXJsIjoiaHR0cHM6Ly9leGFtcGxlLmNvbT9wYXJhbTE9MTIzIiwidXJsMiI6Imh0dHBzOi8vZXhhbXBsZS5jb20/cGFyYW0xPTMyMSJ9.v4jQ4w1rr5AYr0U_bAUv5Swn41NIuENvykhyt52NpX8"
-		expect(jwtUrl).toEqual(base64url)
-		expect(jwtStandard).toEqual(base64std)
-		expect(verifier && await verifier.verify(base64url)).toBeTruthy()
-		expect(verifier && await verifier.verify(base64std)).toBeTruthy()
-	})
-	it("Verifying both standard base64 encoded and url base 64 encoded jwt with old and new issuers (no '/').", async () => {
-		const json = {
-			"prop": "some value not including equal character",
-		}
-		const jwtUrl = validIssuer && await validIssuer.sign(json, 1570094329996)
-		const jwtStandard = validIssuer && await validIssuer.sign(json, 1570094329996, true)
-		expect(jwtUrl).toEqual(jwtStandard)
-		expect(verifier && await verifier.verify(jwtUrl)).toBeTruthy()
-		expect(verifier && await verifier.verify(jwtStandard)).toBeTruthy()
-	})
 })
