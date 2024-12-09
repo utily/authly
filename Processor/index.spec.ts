364ccf58a504bb4921713fd6061ac273ae9bd5aa
Elias Eriksson <eliaseriksson95@gmail.com>
2024-12-09 16:49:01 +0100

    reimplemented one test

--- Processor/index.spec.ts
+++ Processor/index.spec.ts
@@ -51,41 +51,14 @@ const processor = authly.Processor.create(configuration)
 describe("Processor", () => {
 	it("encode", async () => expect(await processor.encode(claims)).toEqual(payload))
 	it("decode", async () => expect(await processor.decode(payload)).toEqual(claims))
-	// it("Transform Both Ways", async () => {
-	// 	expect(await converter.reverse(await converter.apply(claims))).toEqual(claims)
-	// })
-	// it("empty string <---> empty object", async () => {
-	// 	// this conversion is possible with utily/flagly
-	// 	const map: authly.Property.Converter.Configuration<{ flagly: string }, { flagly: Record<string, any> }> = {
-	// 		flagly: {
-	// 			encode: () => ({}),
-	// 			decode: () => "",
-	// 		},
-	// 	}
-	// 	const converter = new authly.Property.Converter(map)
-	// 	expect(await converter.apply({ flagly: "" })).toEqual({ flagly: {} })
-	// })
-	// it("Only encode", async () => {
-	// 	const converter = new authly.Property.Converter<{ foo: number }, { foo: string }>({
-	// 		foo: {
-	// 			encode: value => value.toString(),
-	// 		},
-	// 	})
-	// 	const source = { foo: 123 }
-	// 	const target = await converter.apply(source)
-	// 	expect(target).toEqual({ foo: "123" })
-	// 	expect(await converter.reverse(target)).toEqual(target)
-	// })
-	// it("Only decode", async () => {
-	// 	const converter = new authly.Property.Converter<{ foo: number }, { foo: string }>({
-	// 		foo: {
-	// 			decode: value => parseFloat(value),
-	// 		},
-	// 	})
-	// 	const target = { foo: "123" }
-	// 	const source = await converter.reverse(target)
-	// 	expect(source).toEqual({ foo: 123 })
-	// 	expect(target).toEqual({ foo: "123" })
-	// 	expect(await converter.apply(source)).toEqual(source)
-	// })
+	it("empty string <---> empty object", async () => {
+		type Map = authly.Processor.Type<{
+			flagly: { name: "flagly"; claim: Record<string, never>; payload: string }
+		}>
+		const processor = authly.Processor.create<Map>({
+			flagly: { name: "flagly", encode: () => "", decode: () => ({}) },
+		})
+		expect(await processor.encode({ flagly: {} })).toEqual({ flagly: "" })
+		expect(await processor.decode({ flagly: "" })).toEqual({ flagly: {} })
+	})
 })
