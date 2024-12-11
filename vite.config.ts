a8c10bf4a50f566ce7abb72e8b1c60d1ca22e18a
Simon Mika <simon@mika.se>
2024-12-11 17:18:38 +0100

    Initial setup.

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+/// <reference types="vitest/config" />
+import { defineConfig } from "vite"
+
+export default defineConfig({
+	test: {
+		typecheck: {
+			tsconfig: "./tsconfig.test.json",
+		},
+		globals: true,
+		include: ["**/*.spec.[tj]s"],
+		testTimeout: 20000,
+		isolate: false,
+		exclude: ["node_modules", "dist"],
+		server: {
+			deps: {
+				inline: ["cryptly", "isly", "isoly", "typedly"],
+			},
+		},
+	},
+})
